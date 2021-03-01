0cc9bbc4e459a65a9557c5b2e93af67af42930de
freeivanzw <freeivanzw@gmail.com>
2021-03-01 04:26:44 +0200

    header slider

--- app/js/script.js
+++ app/js/script.js
@@ -2,6 +2,7 @@ $(function(){
     $(".navigation__toggle").click(function(){
         $(".navigation").toggleClass("navigation-active");
         $(".navigation__toggle").toggleClass("toggle-active");
-        
     })
+
+    $(".header__slider").slick()
 });
